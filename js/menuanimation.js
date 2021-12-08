9aa2fe614aa153906f59a02ae656cae79b49c6a9
Guilherme Deus <guil0294@stud.kea.dk>
2021-12-08 17:19:20 +0100

    working in the input (index)

---
+++ js/menuanimation.js
@@ -0,0 +1,17 @@
+
+
+document.querySelector("#HamburguerSymbol").addEventListener("click", menuanimation)
+
+
+
+
+function menuanimation() {
+
+    console.log("open");
+    document.querySelector("nav").classList.toggle("showup");
+
+    document.querySelector("#MenuHome").classList.toggle("ShowMenu");
+    document.querySelector("#MenuCards").classList.toggle("ShowMenu");
+    document.querySelector("#MenuDevlog").classList.toggle("ShowMenu");
+    document.querySelector("#MenuFAQ").classList.toggle("ShowMenu");
+}
