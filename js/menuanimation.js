c041e8f84203ccde290b9e5c041e61abce075164
Guilherme Deus <guil0294@stud.kea.dk>
2021-12-15 18:24:24 +0100

    menu and main changes

--- js/menuanimation.js
+++ js/menuanimation.js
@@ -9,9 +9,4 @@ function menuanimation() {
     console.log("open");
     document.querySelector("nav").classList.toggle("Shownav");
 
-    document.querySelector("#MenuHome").classList.toggle("ShowMenu");
-    document.querySelector("#MenuCards").classList.toggle("ShowMenu");
-    document.querySelector("#MenuDevlog").classList.toggle("ShowMenu");
-    document.querySelector("#MenuFAQ").classList.toggle("ShowMenu");
-
 }
